7da6857e22c8a93ed0f56a1a4dc2af9294848977
Alex Wilson <alex@cooperi.net>
2011-05-25 21:45:15 +1000

    Adding start of proxy model code

---
+++ js/datamodel.js
@@ -0,0 +1,97 @@
+/**
+ * @author Alex Wilson
+ */
+
+var Semester = Class.create(Resource, {
+	cache: {}
+});
+Semester.get = function(id, callback) {
+	Resource.get(Semester, '/semester/'+id+'.json', id, callback);
+}
+
+var Program = Class.create(Resource, {
+	cache: {}
+});
+Program.get = function(id, callback) {
+	Resource.get(Program, '/program/'+id+'.json', id, callback);
+}
+
+var Plan = Class.create(Resource, {
+	cache: {}
+});
+Plan.get = function(id, callback) {
+	Resource.get(Plan, '/plan/'+id+'.json', id, callback);
+}
+
+var CourseGroup = Class.create(Resource, {
+	cache: {}
+});
+CourseGroup.get = function(id, callback) {
+	Resource.get(CourseGroup, '/coursegroup/'+id+'.json', id, callback);
+}
+
+var Course = Class.create(Resource, {
+	cache: {}
+});
+Course.get = function(code, callback) {
+	Resource.get(Course, '/course/'+code+'.json', code, callback);
+}
+
+var Offering = Class.create(Resource, {
+	cache: {},
+	setData: function(data) {
+		$super(data);
+		
+		var ats = data.assessment_tasks;
+		for (var i = 0; i < ats.length; i++) {
+			var a = new AssessmentTask();
+			ats[i].offering = {};
+			ats[i].offering.id = this.getId();
+			a.setData(ats[i]);
+			AssessmentTask.cache[a.getId()] = a; 
+		}
+		
+		var tts = data.series;
+		for (var i = 0; i < tts.length; i++) {
+			var s = new TimetableSeries();
+			tts[i].offering = {};
+			tts[i].offering.id = this.getId();
+			s.setData(tts[i]);
+			TimetableSeries.cache[s.getId()] = s;
+		}
+	}
+});
+Offering.get = function(id, callback) {
+	Resource.get(Offering, '/offering/'+id+'.json', id, callback);
+}
+
+var AssessmentTask = Class.create(Resource, {
+	cache: {}
+});
+AssessmentTask.get = function(id, callback) {
+	if (AssessmentTask.cache[id])
+		callback(AssessmentTask.cache[id]);
+	else
+		callback(null);
+}
+
+var TimetableSeries = Class.create(Resource, {
+	cache: {},
+	setData: function(data) {
+		$super(data);
+	}
+});
+TimetableSeries.get = function(id, callback) {
+	if (TimetableSeries.cache[id])
+		callback(TimetableSeries.cache[id]);
+	else
+		callback(null);
+}
+
+Program.relations = [has_n(Plan, "plans")];
+Plan.relations = [belongs_to(Program, "program"), has_n(CourseGroup, "course_groups")];
+CourseGroup.relations = [belongs_to(Plan, "plan"), has_n(Course, "courses", 'code')];
+Course.relations = [has_n(Course, "prereqs", 'code'), has_n(Course, "dependents", 'code'), has_n(Offering, "offerings")];
+Offering.relations = [belongs_to(Course, "course"), belongs_to(Semester, "semester"), has_n(TimetableSeries, "series"), has_n(AssessmentTask, "assessment_tasks")];
+AssessmentTask.relations = [belongs_to(Offering, "offering")];
+TimetableSeries.relations = [belongs_to(Offering, "offering")];
---
+++ js/model.js
@@ -0,0 +1,70 @@
+/**
+ * @author Alex Wilson
+ */
+
+var Resource = Class.create({
+	initialize: function() {
+		this.data = {};
+	},
+	setData: function(data) {
+		this.data = data;
+		for (x in data) {
+			this['get' + x.capitalize()] = function() {
+				return this.data[x];
+			}
+			this['set' + x.capitalize()] = function(v) {
+				this.data[x] = v;
+			}
+		}
+		
+		for (var i = 0; i < this.relations.length; i++) {
+			var r = this.relations[i];
+			if (r.type == 'belongs_to') {
+				this['get' + r.attr.capitalize()] = function(callback) {
+					r.klass.get(this.data[r.attr][r.key], callback);
+				}
+			} else {
+				this['get' + r.attr.capitalize()] = function(callback) {
+					var rels = this.data[r.attr];
+					var rel_objs = [];
+					for (var i = 0; i < rels.length; i++) {
+						r.klass.get(rels[i][r.key], function (reli) {
+							rel_objs[rel_objs.length] = reli;
+							if (rel_objs.length == rels.length) {
+								callback(rel_objs);
+							}
+						});
+					}
+				}
+			}
+		}
+	}
+});
+
+Resource.get = function(klass, url, id, callback) {
+	if (klass.cache[id]) {
+		callback(klass.cache[id]);
+	} else {
+		new Ajax.Request(url, {
+			onSuccess: function(response) {
+				var o = new klass();
+				o.setData(response.responseJSON);
+				this.cache[id] = o;
+				callback(o);
+			},
+			onFailure: function (response) {
+				callback(null);
+			}
+		});
+	}
+}
+
+var belongs_to = function(klass, attr, key) {
+	key = typeof(key) != 'undefined' ? key : 'id';
+	return { "klass": klass, "attr": attr, "key": key, "type": 'belongs_to' };
+}
+
+var has_n = function(klass, attr, key) {
+	key = typeof(key) != 'undefined' ? key : 'id';
+	return { "klass": klass, "attr": attr, "key": key, "type": 'has_n' };
+}
