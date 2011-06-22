5f753a5104f0d9675c5fb0db61fdfd5809bf4018
Alex Wilson <nikosai@gmail.com>
2011-06-22 16:02:20 +1000

    Continuing work on signup form, adding timetoken support

--- js/lib/API.js
+++ js/lib/API.js
@@ -80,19 +80,36 @@ API.logout = function(callback) {
 }
 
 /**
- * API.register(email, pass[, callback]) -> null
+ * API.getTimeToken(callback) -> null
+ * - callback (function(object))
+ * 
+ * Requests a new time token from the server
+ **/
+API.getTimeToken = function(callback) {
+	new Ajax.Request('/user/timetoken.json', {
+		method: 'get',
+		evalJSON: 'force',
+		onSuccess: function(t) {
+			callback(t.responseJSON);
+		}
+	});
+}
+
+/**
+ * API.register(email, pass, token[, callback]) -> null
  * - email (string)
+ * - token (object): a time token retrieved by API.getTimeToken()
  * - pass (string)
  * - callback (function(bool))
  * 
  * Attempts to register as a new user and create a session
  * as the new login.
  **/
-API.register = function(email, pass, callback) {
+API.register = function(email, pass, token, callback) {
 	new Ajax.Request('/user/me.json', {
 		method: 'put',
 		evalJSON: 'force',
-		parameters: { "user[email]": email, "user[password]": pass },
+		parameters: { "user[email]": email, "user[password]": pass, "tdata": token.tdata, "hash": token.hash },
 		onSuccess: function(t) {
 			if (t.responseJSON.success) {
 				API.secret = t.responseJSON.secret;
